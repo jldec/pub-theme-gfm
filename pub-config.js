4dca8094d16923ceb5bc8e658cd74e61c4700e3e
Jurgen Leschner <jldec@ciaosoft.com>
2015-05-22 15:38:05 -0400

    minor file renames

--- pub-config.js
+++ pub-config.js
@@ -1,13 +1,14 @@
 module.exports =
 {
+  'pub-pkg': 'pub-theme-gfm',
+
   sources: [
     './templates'
   ],
 
   staticPaths: [
     { path:'./node_modules/github-markdown-css/github-markdown.css', route:'/css', maxAge:'1d' },
-    { path:'./static/css', route:'/css', maxAge:'1d' },
-    { path:'./static/js', route:'/js', maxAge:'30d' }
+    { path:'./static' }
   ],
 
 }
